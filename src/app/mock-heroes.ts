e2f25df39644693da0bfd9b9a4198220e411c25b
jinzixiang <jinzixiang1995@gmail.com>
2022-03-24 21:29:28 +0800

    5 files modified

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
-export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+export const ABC: Hero[] = [
+  { id: 11, name: 'Dr Nice' ,gender:"F"},
+  { id: 12, name: 'Narco' ,gender:"M"},
+  { id: 13, name: 'Bombasto' ,gender:"M"},
+  { id: 14, name: 'Celeritas' ,gender:"M"},
+  { id: 15, name: 'Magneta' ,gender:"F"},
+  { id: 16, name: 'RubberMan' ,gender:"M"},
+  { id: 17, name: 'Dynama',gender:"M"},
+  { id: 18, name: 'Dr IQ',gender:"F"},
+  { id: 19, name: 'Magma',gender: "M"},
+  { id: 20, name: 'Tornado',gender:"F" }
 ];
